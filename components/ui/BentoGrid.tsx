00800e7ebb92ac005c25cfe3fbf0382a5e0aa820
thamim1998 <thamim1997@gmail.com>
2025-03-21 23:08:03 +0100

    Globe completed

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,9 @@
 import { cn } from "@/lib/utils";
+import { BackgroundGradientAnimation } from "./BackgroundGradientAnimation";
+import { GridGlobe } from "./GridGlobe";
+import { Globe } from "./Globe";
+import Lottie from "react-lottie";
+import { useState } from "react";
 
 export const BentoGrid = ({ className, children }: { className?: string; children?: React.ReactNode }) => {
   return <div className={cn("grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto", className)}>{children}</div>;
@@ -25,21 +30,107 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+
+  const [copied, setCopied] = useState(false)
+
   return (
     <div
-      className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
-        className
+    className={cn(
+      "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
+      className
+    )}
+    style={{
+      background: "rgb(4,7,29)",
+      backgroundColor:
+        "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+    }}
+  >
+    <div className={`${id === 6 && "flex justify-center"} h-full`}>
+      <div className="w-full h-full absolute">
+        {img && (
+          <img
+            src={img}
+            alt={img}
+            className={cn(imgClassName, "object-cover object-center ")}
+          />
+        )}
+      </div>
+      <div
+        className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"
+          } `}
+      >
+        {spareImg && (
+          <img
+            src={spareImg}
+            alt={spareImg}
+            className="object-cover object-center w-full h-full"
+          />
+        )}
+      </div>
+      {id === 6 && (
+        <BackgroundGradientAnimation>
+          <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
+        </BackgroundGradientAnimation>
       )}
-      style={{
-        background: " rgb(2,0,36)",
-        backgroundColor: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(30,30,156,1) 35%, rgba(0,212,255,1) 100%)",
-      }}
-    >
-      <div className="transition duration-200 group-hover/bento:translate-x-2">
-        <div className="mt-2 mb-2 p-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">{title}</div>
-        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">{description}</div>
+
+      <div
+        className={cn(
+          titleClassName,
+          "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+        )}
+      >
+        <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
+          {description}
+        </div>
+     
+        <div
+          className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+        >
+          {title}
+        </div>
+
+        {id === 2 && <GridGlobe />}
+
+        {id === 3 && (
+          <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:right-2">
+            <div className="flex flex-col gap-3 lg:gap-8">
+              {['React.js', 'Node.js', 'Typescript'].map((item) => (
+                <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                  {item}
+                </span>
+              ))}
+              <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
+
+              </div>
+              <div className="flex flex-col gap-3 lg:gap-8">
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
+              {['Java', 'Spring', 'Python'].map((item) => (
+                <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                  {item}
+                </span>
+              ))}
+
+              </div>
+
+            </div>
+            
+        )}
+
+        {id === 6 && (
+          <div className="mt-5 relative ">
+            <div className={`absolute -bottom-5 right-0`}>
+              <Lottie options={{
+                loop: copied,
+                autoplay: copied,
+                animationData: animationData
+
+              }} />
+              </div>
+          </div>
+        )}
+        
       </div>
     </div>
+  </div>
   );
 };
